a11b09cf535439acbc3e751f2803f6957e7f9c9f
Lucas <lucaschmain@gmail.com>
2024-11-21 21:54:48 -0300

    refatoring: refatoring get coordinates services function to class

--- backend/src/domain/rides/application/services/get-coordinates-service.ts
+++ backend/src/domain/rides/application/services/get-coordinates-service.ts
@@ -9,31 +9,6 @@ interface Coordinates {
 
 interface convertAddressToCoordinatesResponse extends Coordinates {}
 
-async function convertAddressToCoordinates(
-  address: string,
-): Promise<convertAddressToCoordinatesResponse> {
-  const googleMapsApiKey = 'AIzaSyBb43btE7llvofiBSvGJV9A6IzJYk70BtY'
-
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${googleMapsApiKey}`
-
-  try {
-    const response = await axios.get(url)
-
-    if (response.data.status === 'OK') {
-      const location = response.data.results[0].geometry.location
-
-      return {
-        latitude: location.lat,
-        longitude: location.lng,
-      }
-    } else {
-      throw new Error(`Geocoding error: ${response.data.status}`)
-    }
-  } catch (error) {
-    throw new GetCoordinatesError(error)
-  }
-}
-
 interface GetCoordinatesServiceProps {
   originAddress: string
   destinationAddress: string
@@ -44,21 +19,50 @@ interface GetCoordinatesServiceResponse {
   destinationCoordinates: Coordinates
 }
 
-export async function getCoordinatesService({
-  originAddress,
-  destinationAddress,
-}: GetCoordinatesServiceProps): Promise<GetCoordinatesServiceResponse> {
-  try {
-    const originCoordinates = await convertAddressToCoordinates(originAddress)
-    const destinationCoordinates =
-      await convertAddressToCoordinates(destinationAddress)
+export class GetCoordinatesService {
+  private static async convertAddressToCoordinates(
+    address: string,
+  ): Promise<convertAddressToCoordinatesResponse> {
+    const googleMapsApiKey = 'AIzaSyBb43btE7llvofiBSvGJV9A6IzJYk70BtY'
 
-    return {
-      originCoordinates,
-      destinationCoordinates,
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${googleMapsApiKey}`
+
+    try {
+      const response = await axios.get(url)
+
+      if (response.data.status === 'OK') {
+        const location = response.data.results[0].geometry.location
+
+        return {
+          latitude: location.lat,
+          longitude: location.lng,
+        }
+      } else {
+        throw new Error(`Geocoding error: ${response.data.status}`)
+      }
+    } catch (error) {
+      throw new GetCoordinatesError(error)
     }
-  } catch (error) {
-    throw new GetCoordinatesError(error)
+  }
+
+  static async execute({
+    originAddress,
+    destinationAddress,
+  }: GetCoordinatesServiceProps): Promise<GetCoordinatesServiceResponse> {
+    try {
+      const originCoordinates =
+        await this.convertAddressToCoordinates(originAddress)
+
+      const destinationCoordinates =
+        await this.convertAddressToCoordinates(destinationAddress)
+
+      return {
+        originCoordinates,
+        destinationCoordinates,
+      }
+    } catch (error) {
+      throw new GetCoordinatesError(error)
+    }
   }
 }
 
